d0431f68c86a88363f9133cfe4655fd52810734f
Tarek Hallak <HallakTarek7@gmail.com>
2024-06-10 17:47:23 +0300

    Build the api file

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\InvoiceController;
+use App\Http\Controllers\Api\V1\CustomerController;
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +21,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1' , 'namespace' => 'App\Http\Controllers\Api\V1'], function(){
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
 });
