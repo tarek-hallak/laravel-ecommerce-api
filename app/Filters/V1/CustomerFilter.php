4a47e5f15ca3f10f90a115ec7b84b9106ce862eb
Tarek Hallak <HallakTarek7@gmail.com>
2024-06-14 04:18:18 +0300

    delete: delete transform function.

--- app/Filters/V1/CustomerFilter.php
+++ app/Filters/V1/CustomerFilter.php
@@ -28,23 +28,4 @@ class CustomerFilter extends ApiFilter {
         'lte' => '<='
     ];
 
-    public function transform(Request $request){
-        $eloQeury =[];
-
-        foreach($this->safePrams as $parm => $operators){
-            $query = $request->query($parm);
-            if(!isset($query)){
-                continue;
-            }
-            $column = $this->columnMap[$parm] ?? $parm ;
-
-            foreach($operators as $operator){
-                if(isset($query[$operator])){
-                    $eloQeury[] = [$column,$this->operatorMap[$operator],$query[$operator]];
-                }
-            }
-        }
-
-        return $eloQeury;
-    }
 }
