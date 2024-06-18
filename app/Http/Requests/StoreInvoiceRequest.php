289e14ee81d81b3c0a2283f058ce64456d45235b
Tarek Hallak <HallakTarek7@gmail.com>
2024-06-18 03:32:20 +0300

    feuture: protecting routs with sanctum(Tokens).

--- app/Http/Requests/StoreInvoiceRequest.php
+++ app/Http/Requests/StoreInvoiceRequest.php
@@ -11,7 +11,8 @@ class StoreInvoiceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        $user = $this->user();
+        return $user != null && $user->tokenCan('create');    
     }
 
     /**
